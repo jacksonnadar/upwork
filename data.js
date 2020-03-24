4ccdf07651103a2f5c318fedd7346fe49a6c19ce
jacksonnadar <jacksonnadar123@gmail.com>
2020-03-24 13:17:53 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,141 @@
+let data_object = [
+  { title: "Chemistry", location: "On Campus", degree: "BA, BS, Minor" },
+  { title: "Accounting", location: "On Campus", degree: "AA, BS, Minor" },
+  { title: "Anthropology", location: "On Campus", degree: "BS, Minor" },
+  {
+    title: "Associate in Philosophy Online",
+    location: "Online",
+    degree: "Associate"
+  },
+  {
+    title: "Associate in Theology Online",
+    location: "Online",
+    degree: "Associate"
+  },
+  { title: "Biochemistry", location: "On Campus", degree: "BA" },
+  { title: "Bioinformatics", location: "On Campus", degree: "Certification" },
+  { title: "Biology", location: "On Campus", degree: "BA, BS, Minor" },
+  {
+    title: "Canon Law First Cycle",
+    location: "On Campus Online",
+    degree: "Special"
+  },
+  { title: "Catechetics", location: "On Campus", degree: "BA" },
+  {
+    title: "Catechetics Specialization (MA Theology & Christian Ministry)",
+    location: "On Campus",
+    degree: "Master"
+  },
+
+  { title: "Classical Latin Minor", location: "On Campus", degree: "Minor" },
+  { title: "Classics", location: "On Campus", degree: "BA" },
+  { title: "Communication Arts", location: "On Campus", degree: "BA, Minor" },
+  { title: "Computer Science", location: "On Campus", degree: "BS, Minor" },
+  { title: "Criminal Justice", location: "On Campus", degree: "BA" },
+  { title: "Criminology Minor", location: "On Campus", degree: "Minor" },
+  { title: "Economics", location: "On Campus", degree: "BA, BS, Minor" },
+  { title: "Education", location: "On Campus", degree: "BS" },
+  { title: "Engineering", location: "On Campus", degree: "AA, BS" },
+  { title: "English", location: "On Campus", degree: "BA, Minor" },
+  { title: "Exercise Science Minor", location: "On Campus", degree: "Minor" },
+  { title: "Film Studies Minor", location: "On Campus", degree: "Minor" },
+  { title: "Finance", location: "On Campus", degree: "BS, Minor" },
+  { title: "Franciscan Studies Minor", location: "On Campus", degree: "Minor" },
+  { title: "Greek Minor", location: "On Campus", degree: "Minor" },
+  { title: "History", location: "On Campus", degree: "BA, Minor" },
+  { title: "Honors Program", location: "On Campus", degree: "Special" },
+  {
+    title: "Human Development and Family Studies",
+    location: "On Campus",
+    degree: "BA"
+  },
+  { title: "Human Life Studies Minor", location: "On Campus", degree: "Minor" },
+  {
+    title: "Humanities and Catholic Culture",
+    location: "On Campus",
+    degree: "BA"
+  },
+  {
+    title: "International Business",
+    location: "On Campus",
+    degree: "BS, Minor"
+  },
+  {
+    title: "Law School Preparation",
+    location: "On Campus",
+    degree: "BA, Minor"
+  },
+  { title: "Legal Studies Minor", location: "On Campus", degree: "Minor" },
+  {
+    title: "MA Catechetics and Evangelization",
+    location: "Online",
+    degree: "Master"
+  },
+  {
+    title: "MA Clinical Mental Health Counseling",
+    location: "On Campus",
+    degree: "Master"
+  },
+  { title: "MA in Catholic Studies", location: "Online", degree: "Master" },
+  { title: "MA Philosophy", location: "On Campus", degree: "Master" },
+  {
+    title: "MA Theological Studies",
+    location: "On CampusOnline",
+    degree: "Master"
+  },
+  {
+    title: "MA Theology & Christian Ministry",
+    location: "On CampusOnline",
+    degree: "Master"
+  },
+  { title: "MA Theology 4+1 Program", location: "On Campus", degree: "Master" },
+  { title: "Management", location: "On Campus", degree: "BS, Minor" },
+  { title: "Marketing", location: "On Campus", degree: "BS, Minor" },
+  {
+    title: "Master of Catholic Leadership",
+    location: "Online",
+    degree: "Master"
+  },
+  {
+    title: "Master of Theological Science",
+    location: "On Campus",
+    degree: "Master"
+  },
+  { title: "Mathematical Science", location: "On Campus", degree: "BS, Minor" },
+  { title: "MBA Online", location: "Online", degree: "Master" },
+  { title: "Modern Languages", location: "On Campus", degree: "BA, Minor" },
+  { title: "MS Education Online", location: "Online", degree: "Master" },
+  { title: "MS Nursing Program", location: "On Campus", degree: "Master" },
+  { title: "Music Ministry Minor", location: "On Campus", degree: "Minor" },
+  { title: "Narrative Arts Minor", location: "On Campus", degree: "Minor" },
+  { title: "Nursing", location: "On Campus", degree: "BS" },
+  {
+    title: "Pharmacy Program (2+4 or 3+4 Doctorate Program)",
+    location: "On Campus",
+    degree: "AA, BS"
+  },
+  { title: "Philosophy", location: "On Campus", degree: "AA, BA, Minor" },
+  { title: "Political Science", location: "On Campus", degree: "BA, Minor" },
+  {
+    title: "Pre-Professional Programs",
+    location: "On Campus",
+    degree: "Special"
+  },
+  {
+    title: "Psychology – Clinical",
+    location: "On Campus",
+    degree: "BA, Concentration"
+  },
+  {
+    title: "Psychology – Experimental",
+    location: "On Campus",
+    degree: "BA, BS, Concentration"
+  },
+  { title: "Sacred Music", location: "On Campus", degree: "BA, Minor" },
+  { title: "Social Work", location: "On Campus", degree: "BS" },
+  { title: "Sociology", location: "On Campus", degree: "BA, Minor" },
+  { title: "Theatre", location: "On Campus", degree: "BA, Minor" },
+  { title: "Theology", location: "On Campus", degree: "AA, BA, Minor" },
+  { title: "Western & World Literature", location: "On Campus", degree: "BA" },
+  { title: "Youth Ministry Minor", location: "On Campus", degree: "Minor" }
+];
